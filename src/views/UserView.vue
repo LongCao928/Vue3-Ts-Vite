ffac99332072fe7512a4a4431d03e5cd8d10542b
LongCao928 <17512055928@163.com>
2023-04-14 20:51:47 +0800

    style

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="user">user</div>
+  <div class="user">
+    <span>user</span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
   .user {
     font-size: 20px;
-    color: red;
+    position: relative;
+    span {
+      position: absolute;
+      float: left;
+      left: 0;
+      top: 0;
+    }
   }
 </style>
