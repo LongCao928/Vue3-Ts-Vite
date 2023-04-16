39ca4cc71fbccaa08d24629bbad6ff3e0936d179
LongCao928 <17512055928@163.com>
2023-04-16 18:43:38 +0800

    element-plus 按需加载

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// import ElementPlus from 'element-plus'
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router/index'
@@ -11,7 +12,13 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(ElementPlus)
+
+/* app.use(ElementPlus, {
+  locale: zhCn, // 默认使用英语，更改为中文
+  size: 'small', // 设置表单组件默认尺寸
+  zIndex: 3000, // 设置弹出组件的层级，默认2000
+}) */
+
 app.use(router)
 
 app.mount('#app')
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const value = ref('')
+</script>
 
 <template>
   <!-- <div class="user">
@@ -7,6 +10,7 @@
     <el-card class="card">
       <template #header>User</template>
       <el-text size="large" type="primary">This is content...</el-text>
+      <el-input v-model="value"></el-input>
     </el-card>
 </template>
 
